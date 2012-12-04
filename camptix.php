Automattic/camptix

commit fe9fa60ab9c79263c8a9e732066fdd9ae77680c5

Use numeric values in class names instead of sanitized questions when rendering registration forms. Allows to target non-English questions fields with CSS. Props @maor see #18

camptix.php
@@ -4220,7 +4220,10 @@ class CampTix_Plugin {
 									</td>
 								</tr>
 
-								<?php do_action( 'camptix_question_fields_init' ); ?>
+								<?php
+									do_action( 'camptix_question_fields_init' );
+									$i = 0; // Used for questions class names.
+								?>
 								<?php foreach ( $questions as $question ) : ?>
 
 									<?php
@@ -4228,8 +4231,9 @@ class CampTix_Plugin {
 										$name = sprintf( 'tix_attendee_questions[%d][%s]', $i, $question_key );
 										$value = isset( $this->form_data['tix_attendee_questions'][$i][$question_key] ) ? $this->form_data['tix_attendee_questions'][$i][$question_key] : '';
 										$question_type = $question['type'];
+										$class_name = 'tix-row-question-' . ++$i;
 									?>
-									<tr class="tix-row-<?php echo $question_key; ?>">
+									<tr class="<?php echo esc_attr( $class_name ); ?>">
 										<td class="<?php if ( $question['required'] ) echo 'tix-required'; ?> tix-left"><?php echo esc_html( $question['field'] ); ?><?php if ( $question['required'] ) echo ' <span class="tix-required-star">*</span>'; ?></td>
 										<td class="tix-right">
 											<?php do_action( "camptix_question_field_$question_type", $name, $value, $question ); ?>

commit eade88ad68cf66f1865dcd2b14e52a805cb35769

The $i in fe9fa60ab9c79263c8a9e732066fdd9ae77680c5 broke questions numbering. Use a different variable name.

camptix.php
@@ -4222,7 +4222,7 @@ class CampTix_Plugin {
 
 								<?php
 									do_action( 'camptix_question_fields_init' );
-									$i = 0; // Used for questions class names.
+									$question_num = 0; // Used for questions class names.
 								?>
 								<?php foreach ( $questions as $question ) : ?>
 
@@ -4231,7 +4231,7 @@ class CampTix_Plugin {
 										$name = sprintf( 'tix_attendee_questions[%d][%s]', $i, $question_key );
 										$value = isset( $this->form_data['tix_attendee_questions'][$i][$question_key] ) ? $this->form_data['tix_attendee_questions'][$i][$question_key] : '';
 										$question_type = $question['type'];
-										$class_name = 'tix-row-question-' . ++$i;
+										$class_name = 'tix-row-question-' . ++$question_num;
 									?>
 									<tr class="<?php echo esc_attr( $class_name ); ?>">
 										<td class="<?php if ( $question['required'] ) echo 'tix-required'; ?> tix-left"><?php echo esc_html( $question['field'] ); ?><?php if ( $question['required'] ) echo ' <span class="tix-required-star">*</span>'; ?></td>