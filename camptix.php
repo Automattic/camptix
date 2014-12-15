397760fee05996581256a90cf49c4f46b63c23ff
Ian Dunn <ian@iandunn.name>
2014-12-15 15:48:44 -0800

    Bump version to 1.4.2.
    This was accidentally left out of a013a4b365e4adf2c47887507c2916cde671d671.

--- camptix.php
+++ camptix.php
@@ -3,7 +3,7 @@
  * Plugin Name: CampTix Event Ticketing
  * Plugin URI:  http://wordcamp.org
  * Description: Simple and flexible event ticketing for WordPress.
- * Version:     1.4.1
+ * Version:     1.4.2
  * Author:      Automattic
  * Author URI:  http://wordcamp.org
  * License:     GPLv2
