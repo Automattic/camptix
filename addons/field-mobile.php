2c65f98487a141e1fe02fb402d453a31fb9a6c50
Vachan Kudmule <vachan@kdc.in>
2017-07-22 00:41:32 +0530

    Create field-mobile.php

---
+++ addons/field-mobile.php
@@ -0,0 +1,45 @@
+
+<?php
+/**
+ * Mobile Field Addon for CampTix
+ *
+ * A few of the payment platforms requie the mobile/phone field to complete the
+ * transction. By adding it as a field, the payment class can explictly call this
+ * field type to pass the variable to the payment platform.
+ *
+ */
+class CampTix_Addon_Mobile_Field extends CampTix_Addon {
+
+	/**
+	 * Register hook callbacks
+	 */
+	function camptix_init() {
+		add_filter( 'camptix_question_field_types',   array( $this, 'question_field_types'   ) );
+		add_action( 'camptix_question_field_mobile', array( $this, 'question_field_mobile' ), 10, 3 );
+	}
+
+	/**
+	 * Add Country to the list of question types.
+	 *
+	 * @param array $types
+	 *
+	 * @return array
+	 */
+	function question_field_types( $types ) {
+		return array_merge( $types, array(
+			'mobile' => 'Mobile',
+		) );
+	}
+
+	/**
+	 * A url input for a question.
+	 */
+	function question_field_mobile( $name, $value ) {
+		?>
+		<input name="<?php echo esc_attr( $name ); ?>" type="url" value="<?php echo esc_attr( $value ); ?>" />
+		<?php
+	}
+}
+
+// Register this class as a CampTix Addon.
+camptix_register_addon( 'CampTix_Addon_Mobile_Field' );
